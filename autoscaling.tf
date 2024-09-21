04a50e70830faac40f34f6ad5da08e3923de6576
Arisson Assis <60162316+AssisArisson@users.noreply.github.com>
2024-09-21 11:18:49 -0300

    impl app scale

---
+++ autoscaling.tf
@@ -0,0 +1,11 @@
+## App Auto Scalling service para escalar qualquer serviço aws.. exemplo DynamoDb, Cassandra etc, verificar doc:
+# https://registry.terraform.io/providers/hashicorp/aws/5.38.0/docs/resources/appautoscaling_target.html
+
+resource "aws_appautoscaling_target" "main" {
+  max_capacity = var.task_maximum
+  min_capacity = var.task_minimum
+
+  resource_id = "service/${var.cluster_name}/${aws_ecs_service.main.name}}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace = "ecs"
+}
